b5517ba554e5fd4bb67222c9140ba5912de95215
Laurent David <lmedavid@gmail.com>
2019-07-01 17:42:40 +0200

    Refactor my courses as part of the new savoir_mycourses block

--- version.php
+++ version.php
@@ -24,11 +24,12 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2018112102; // This is the version number to increment when changes needing an update are made.
+$plugin->version   = 2018112103; // This is the version number to increment when changes needing an update are made.
 $plugin->requires  = 2018051706; // Moodle 3.5.
 $plugin->release   = '1.0.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'theme_savoir';
 $plugin->dependencies = [
-    'theme_boost' => '2018051400'
+    'theme_boost' => '2018051400',
+    'block_savoir_mycourses' => '2018051706',
 ];
